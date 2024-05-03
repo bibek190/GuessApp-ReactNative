2b3d8c30dfa9f9e404809b8bd2522fa5e391eb8d
bibek190 <bibek9818917036@gmail.com>
2024-05-04 01:34:46 +1000

    layouts design

--- App.js
+++ App.js
@@ -1,10 +1,19 @@
 import { StatusBar } from "expo-status-bar";
-import { StyleSheet, Text, View } from "react-native";
+import { StyleSheet, Text, TextInput, View } from "react-native";
 
 export default function App() {
   return (
     <View style={styles.container}>
-      <Text>Hello</Text>
+      <View>
+        <Text>Guess a Number</Text>
+      </View>
+      <View>
+        <TextInput placeholder="00" />
+      </View>
+      <View>
+        <Text>Wrong nuber</Text>
+      </View>
+
       <StatusBar style="auto" />
     </View>
   );
